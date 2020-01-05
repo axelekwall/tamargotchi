49d0af54af33daac3ef7eaebcdd387e4a2dd2315
Axel Ekwall <git@axelekwall.se>
2020-01-06 00:11:14 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
