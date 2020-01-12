ce04114bfca6c634e07b6abe225fe993e91a39fd
Ella <ella.wg@hotmail.com>
2020-01-12 21:04:16 +0100

    add click listener

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log('test');
+function onSceneLoaded() {
+  const raycaster = document.querySelector('[ar-raycaster]');
+  const cursor = document.querySelector('#cursor');
+  raycaster.addEventListener('raycaster-intersection', event => {
+    cursor.setAttribute('position', event.detail.intersections[0].point);
+  });
+}
+
+const scene = document.querySelector('a-scene');
+scene.addEventListener('loaded', onSceneLoaded);
+
+const walker = document.querySelector('#walker');
+raycaster.addEventListener('click', () => {
+  walker.setAttribute(
+    'position',
+    raycaster.components.cursor.intersection.point
+  );
+});
